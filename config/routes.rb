3346f03ea9daf1e8d7cd20e8be35a4ab423dff53
Igor Nichele <>
2020-04-15 20:42:28 -0300

    endpoint search

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get 'home/index'
   root 'home#index'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :dashboard, only: :index
+      resources :categories, only: :index
+      resources :categories, only: :index
+    end
+  end
+
+
 end
